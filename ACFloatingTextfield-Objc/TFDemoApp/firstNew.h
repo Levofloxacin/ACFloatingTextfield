786ef7b5a6d89428838fb71f8347a584d5d0c534
MINTQ-LEE <lee@MINTQ-LEEdeMacBook-Pro.local>
2018-04-18 19:00:34 +0800

    firstnew

---
+++ ACFloatingTextfield-Objc/TFDemoApp/firstNew.h
@@ -0,0 +1,13 @@
+//
+//  firstNew.h
+//  ACFloatingTextField
+//
+//  Created by MINTQ-LEE on 2018/4/18.
+//  Copyright © 2018年 Abhishek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface firstNew : NSObject
+
+@end
---
+++ ACFloatingTextfield-Objc/TFDemoApp/firstNew.m
@@ -0,0 +1,13 @@
+//
+//  firstNew.m
+//  ACFloatingTextField
+//
+//  Created by MINTQ-LEE on 2018/4/18.
+//  Copyright © 2018年 Abhishek. All rights reserved.
+//
+
+#import "firstNew.h"
+
+@implementation firstNew
+
+@end
